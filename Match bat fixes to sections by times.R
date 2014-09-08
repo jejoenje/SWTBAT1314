bf2300b3efb9278edd512ea170c07828318e3e2f
unknown <jm177@STR001D7D572D84.ad.stir.ac.uk>
2014-09-08 17:06:02 +0100

    Many edits to raw data; now working scripts to match fixes to transect sections; in process of checking against GIS.

--- Match bat fixes to sections by times.R
+++ Match bat fixes to sections by times.R
@@ -28,8 +28,8 @@ for (i in 1:length(visits)) {
   }
   sect_out <- rbind(sect_out, vst_out)
 }
-#sect_out$test <- as.numeric(sect_out$e_dtime-sect_out$s_dtime)
-#write.csv(sect_out, 'data/Temp/sect_out.csv', row.names=F)
+# sect_out$test <- as.numeric(sect_out$e_dtime-sect_out$s_dtime)
+# write.csv(sect_out, 'data/Temp/sect_out.csv', row.names=F)
 
 bats <- read.csv('data/SWT 2014 all bat fixes with coords.csv', header=T)
 bats_dt <- paste(bats$Y, bats$M, bats$D, sep='-')
@@ -43,44 +43,19 @@ counts <- c('FLAG','UNK','SOPPIP','COMPIP','MYOTI','PIP','DUNNO','PAUR','NOCTU',
 sect_out[,counts] <- apply(sect_out[,counts],2,as.numeric)
 sect_out[,counts] <- 0
 
-
-for(i in 1:nrow(sect_out)) {
+for (i in 1:nrow(sect_out)) {
   s <- sect_out$s_dtime[i]
   e <- sect_out$e_dtime[i]
-  bats_sect <- bats[bats$SURVEYID==sect_out$SURVEYID[i],]
-  spp <- bats_sect[bats_sect$dtime>s & bats_sect$dtime<e,'Species']
-  notes <- bats_sect[bats_sect$dtime>s & bats_sect$dtime<e,'Note']
-  notes <- toupper(notes)
-  
-  for (j in 1:length(notes[notes!=''])) {
-    
-  }  
-  if (notes!='') {
-    sptable <- data.frame(spp=gsub("[ 0-9]","",trim(unlist(strsplit(notes[1],',')))),
-                          no=as.numeric(gsub("[ A-Z]","",trim(unlist(strsplit(notes[1],','))))))
-    sect_out[i,as.vector(sptable$spp)] <- sptable$no
+  bats_surv <- bats[bats$SURVEYID==sect_out$SURVEYID[i],]
+  bats_sect <- bats_surv[bats_surv$dtime>s & bats_surv$dtime<=e,]
+  if(nrow(bats_sect)>0) {
+    sptab <- data.frame(spp=bats_sect$Species, no=bats_sect$COUNT)
+    sptab <- sptab[sptab$spp %in% counts,]
+    sptab$spp <- factor(as.vector(sptab$spp))
+    sptab <- tapply(sptab$no, sptab$spp, length)
+    sect_out[i,names(sptab)] <- sptab
   }
-
-  
-}
-
-
-sect_out2 <- as.data.frame(NULL)
-for(i in 1:nrow(bats)) {
-  bats_sect <- sect_out[sect_out$SURVEYID==bats$SURVEYID[i],]
-  if (sum((bats$dtime[i]>bats_sect$s_dtime)*(bats$dtime[i]<bats_sect$e_dtime))!=0) {
-    intvl <- which(as.logical((bats$dtime[i]>bats_sect$s_dtime)*(bats$dtime[i]<bats_sect$e_dtime)))  
-    if(bats$Species[i]!='SOCCAL' & bats$Species[i]!='' & bats$Species[i]!='BUZZ') {
-      if(bats$Note[i]=='') {
-        bats_sect[intvl,bats$Species[i]] <- bats_sect[intvl,bats$Species[i]] + 1
-      } else {
-        spp <- gsub("[ 0-9]","",unlist(lapply(strsplit(bats$Note[i],','),trim)))
-        spp <- toupper(spp)
-        no <- as.numeric(gsub("[ a-z]","",unlist(lapply(strsplit(bats$Note[i],','),trim))))
-        bats_sect[intvl,spp] <- bats_sect[intvl,spp] + no
-      }
-    }
-  }
-  sect_out2 <- rbind(sect_out2, bats_sect[intvl,])
   print(i)
 }
+
+write.csv(sect_out, '/data/Bat data per section processed.csv', row.names=F)
