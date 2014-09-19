02fd244e21bee388714cc68d1e0dea9fccff1e6b
unknown <jm177@STR001D7D572D84.ad.stir.ac.uk>
2014-09-19 16:05:39 +0100

    New combined data set with bat pass counts for all sites and visits in 2013 and 2014; started adapting analysis script to start matching habitat data etc.

--- Bats analysis.R
+++ Bats analysis.R
@@ -1,12 +1,25 @@
 library(xlsx)
 library(lattice)
-bats <- read.xlsx('data/Bat data per section.xlsx',sheetIndex=1,header=T)
+bats <- read.xlsx('data/Bat data per section 2013 and 2014.xlsx',sheetIndex=1,header=T)
 
 # Wind speed as numeric:
 bats$WINDS <- as.numeric(as.vector(bats$WINDS))
 
 # fSECTION is transect section as factor:
 bats$fSECTION <- factor(bats$SECTION)
+
+# Make 'truetime' from s_dtime:
+bats$s_dtime <- strptime(bats$s_dtime, format='%Y-%m-%d %H:%M:%S')
+
+# For the 2013 surveys, times were only recorded specific to whole transects.
+# Make sure that the 2014 data is 'downsampled' to this resolution.
+# Construct factor to indicate transect-visit-site:
+bats$SiteTrans <- with(bats, paste(SURVEYID,TRSCT,sep='-'))
+bats$SiteTrans <- factor(bats$SiteTrans)
+# Calculate the earliest time recorded per transect-visit-site:
+temp <- ddply(bats, .(SiteTrans), summarise, startt=min(s_dtime))
+
+
 
 # Calculate time minutes to/since midnight w/i given night.
 # First extract ISO date/time.
@@ -22,6 +35,9 @@ temp3 <- data.frame(temp, as.vector(temp1), as.vector(temp2))
 # Now work out which of the two above differences in minutes is the smallest in absolute terms, and return that
 #  difference.
 bats$ttMidn <- apply(as.matrix(temp3[,2:3]),1,function(x) { x[abs(x)==min(abs(x))] })
+
+
+
 
 # Julian day number since 1 Jan
 bats$jDATE <- round(difftime(temp, as.Date(paste(as.character(format(temp,'%Y')),'-01-01',sep=''),
