70f22b8537658eb4864330336b93940f06fd4c76
unknown <jm177@STR001D7D572D84.ad.stir.ac.uk>
2014-12-17 14:42:40 +0000

    Fitted binomial models, produced predictions. No apparent great effect of section.

--- 04-Bat analysis.R
+++ 04-Bat analysis.R
@@ -25,184 +25,150 @@ tapply(bats$ALL_PIPS, bats$SITE, function(x) sum(x==0)/length(x))
 ### Average proportion of zero's per site:
 mean(tapply(bats$ALL_PIPS, bats$SITE, function(x) sum(x==0)/length(x)))
 
-### Try fitting intercept-only NB GLM per site:
-sitedat <- bats[bats$SITE==levels(bats$SITE)[2],]
-mod_site <- glm.nb(ALL_PIPS ~ fSECTION+WINDS, data=sitedat)
-dispZuur(mod_site)
-plot(fitted(mod_site), resid(mod_site))
+### First date:
+min(as.Date(as.vector(bats$DATE),format='%Y-%m-%d'))
 
+### Last date:
+max(as.Date(as.vector(bats$DATE),format='%Y-%m-%d'))
 
-system.time(mod1 <- glmer(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-              family='poisson', data=bats))
+### Binary responses and standardise predictors:
+bats$OCC_PIPS <- as.numeric(as.logical(bats$ALL_PIPS))
+bats$OCC_OTHR <- as.numeric(as.logical(bats$ALL_OTHER))
+bats$sMINTEMP <- scale(bats$min_air_temp)
+bats$sMINTEMP2 <- bats$sMINTEMP^2
+bats$sWINDS <- scale(bats$WINDS)
+bats$sWINDS2 <- bats$sWINDS^2
+bats$sDAYNO <- scale(bats$DAYNO)
+bats$sDAYNO2 <- bats$sDAYNO^2
+bats$sTTMIDN <- scale(bats$TTMIDN)
+bats$sTTMIDN2 <- bats$sTTMIDN^2
+bats$spBUILD <- scale(bats$pBUILD)
+bats$sD_TRE <- scale(bats$D_TRE)
 
-system.time(mod2 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                 family='nbinom1', data=bats))
+bats_nona <- bats[!is.na(bats$WINDS),]
+bats_nona <- droplevels(bats_nona)
 
-system.time(mod3 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                             family='nbinom1', zeroInfl=T, data=bats))
-
-system.time(mod4 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                             family='poisson', data=bats))
-
-system.time(mod5 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                             family='poisson', zeroInfl=T, data=bats))
-
-AIC(mod1, mod2, mod3, mod4, mod5)
-plot(fitted(mod2),resid(mod2))
-plot(bats$fSECTION,resid(mod2))
-plot(bats$SITE,resid(mod2))
-plot(fitted(mod2),bats$PASSES); abline(a=0, b=1, col='red')
-
-system.time(mod2a <- glmmadmb(PASSES ~ fSECTION + NOTURB + TTMIDN + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                             family='nbinom', data=bats))
-
-
-bats_nona <- bats[!is.na(bats$WINDS),]; bats_nona <- droplevels(bats_nona)
-system.time(mod10 <- glmmadmb(ALL_PIPS ~ fSECTION*NOTURB + 
-                   WINDS + DAYNO + TTMIDN + I(TTMIDN^2) + pTREE + D_LIN + 
-                   (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                 family='nbinom', data=bats_nona))
-plot(fitted(mod10),resid(mod10))
-plot(bats_nona$fSECTION,resid(mod10))
-plot(bats_nona$NOTURB,resid(mod10))
-plot(bats_nona$WINDS,resid(mod10))
-plot(bats_nona$DAYNO,resid(mod10))
-plot(bats_nona$TTMIDN,resid(mod10))
-plot(bats_nona$pTREE,resid(mod10))
-plot(bats_nona$D_LIN,resid(mod10))
-plot(bats_nona$SITE,resid(mod10))
-
-bats_nona$resid10 <- resid(mod10)
-plotbubble(bats_nona$MID_X, bats_nona$MID_Y, bats_nona$resid10)
-
-par(mfrow=c(5,7))
-par(mar=c(0,0,0,0))
-for(i in 1:nlevels(bats_nona$SITE)) {
-  sitedat <- bats_nona[bats_nona$SITE==levels(bats_nona$SITE)[i],]
-  plotbubble(sitedat$MID_X, sitedat$MID_Y, sitedat$resid10, axt='n')
-}
-for(i in 1:nlevels(bats_nona$SITE)) {
-  sitedat <- bats_nona[bats_nona$SITE==levels(bats_nona$SITE)[i],]
-  coordinates(sitedat) <- c('MID_X','MID_Y')
-  vario_site <- variogram(resid10 ~ 1, data=sitedat)
-  plotvario(vario_site, axes=F)
-}
-
-
-bats_nona$sWINDS <- scale(bats_nona$WINDS)
-bats_nona$sDAYNO <- scale(bats_nona$DAYNO)
-bats_nona$sTTMIDN <- scale(bats_nona$TTMIDN)
-bats_nona$spTREE <- scale(bats_nona$pTREE)
-bats_nona$sD_LIN <- scale(bats_nona$D_LIN)
-bats_nona$sTTMIDN2 <- bats_nona$sTTMIDN^2
-bats_nona$sD_LIN2 <- bats_nona$sD_LIN^2
-bats_nona$sWINDS2 <- bats_nona$sWINDS^2
-bats_nona$OCC_PIPS <- as.numeric(as.logical(bats_nona$ALL_PIPS))
-mod90 <- glmer(OCC_PIPS ~ fSECTION*NOTURB + 
-                 sWINDS + sDAYNO + sTTMIDN + spTREE + sD_LIN + 
-                 (1|SITE/TRSCT), 
-               family=binomial, data=bats_nona)
-dispZuur(mod90)
-binnedplot(fitted(mod90), resid(mod90, type='pearson'))
-
-
-system.time(mod11 <- update(mod10, .~. +I(D_LIN^2) +I(WINDS^2)) )
-plot(fitted(mod11),resid(mod11))
-plot(bats_nona$fSECTION,resid(mod11))
-plot(bats_nona$NOTURB,resid(mod11))
-plot(bats_nona$WINDS,resid(mod11))
-plot(bats_nona$DAYNO,resid(mod11))
-plot(bats_nona$TTMIDN,resid(mod11))
-plot(bats_nona$pTREE,resid(mod11))
-plot(bats_nona$D_LIN,resid(mod11))
-plot(bats_nona$SITE,resid(mod11))
-
-AIC(mod10, mod11)
-
-
-
-system.time(mod12 <- glmmadmb(ALL_PIPS ~ fSECTION*NOTURB + 
-                                sWINDS + sWINDS2 + sDAYNO + sTTMIDN + sTTMIDN2 + 
-                                spTREE + sD_LIN + sD_LIN2 + 
-                                (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                              family='nbinom', data=bats_nona))
-plot(fitted(mod12),resid(mod12))
-plot(bats_nona$fSECTION,resid(mod12))
-plot(bats_nona$NOTURB,resid(mod12))
-plot(bats_nona$WINDS,resid(mod12))
-plot(bats_nona$DAYNO,resid(mod12))
-plot(bats_nona$TTMIDN,resid(mod12))
-plot(bats_nona$pTREE,resid(mod12))
-plot(bats_nona$D_LIN,resid(mod12))
-plot(bats_nona$SITE,resid(mod12))
-
-AIC(mod10, mod11, mod12)
-
-par(mfrow=c(1,2))
+mod1 <- glmer(OCC_PIPS ~ fSECTION*NOTURB + 
+                sMINTEMP +
+                sWINDS + 
+                sDAYNO + 
+                sTTMIDN + 
+                spBUILD + 
+                sD_TRE + 
+                (1|SITE/TRSCT), data=bats_nona, family=binomial)
+bats_nona$res1 <- resid(mod1, type='pearson')
+# Diagnostic plots for basic binomial GLMM:
+binnedplot(fitted(mod1), bats_nona$res1)
+plot(as.vector(bats_nona$fSECTION), bats_nona$res1)
+plot(bats_nona$NOTURB, bats_nona$res1)
+plot(bats_nona$sMINTEMP, bats_nona$res1)
+plot(bats_nona$sWINDS, bats_nona$res1)
+plot(bats_nona$sDAYNO, bats_nona$res1)
+plot(bats_nona$sTTMIDN, bats_nona$res1)
+plot(bats_nona$spBUILD, bats_nona$res1)
+plot(bats_nona$sD_TRE, bats_nona$res1)
+plot(predict(mod1, type='response'),jitter(bats_nona$OCC_PIPS, 0.05))
 dispZuur(mod1)
-plot(fitted(mod1),resid(mod1))
+# 'Error rate':
+sum(as.numeric(predict(mod1, type='response')>0.5)!=bats_nona$OCC_PIPS)/nrow(bats_nona)
+
+# Need quad term for wind?
+mod1a <- update(mod1, .~. +sWINDS2)
+anova(mod1, mod1a) # No.
+
+# Quad term for TTMIDN?
+mod1a <- update(mod1, .~. +sTTMIDN2)
+anova(mod1, mod1a) # Yes!
+
+# Quad term for DAYNO?
+mod1b <- update(mod1a, .~. +sDAYNO2)
+anova(mod1a, mod1b) # Yes?
+
+# New 'full' model:
+mod2 <- mod1b
+bats_nona$res2 <- resid(mod2, type='pearson')
+binnedplot(fitted(mod2), bats_nona$res2)
+plot(as.vector(bats_nona$fSECTION), bats_nona$res2)
+plot(bats_nona$NOTURB, bats_nona$res2)
+plot(bats_nona$sMINTEMP, bats_nona$res2)
+plot(bats_nona$sWINDS, bats_nona$res2)
+plot(bats_nona$sDAYNO, bats_nona$res2)
+plot(bats_nona$sTTMIDN, bats_nona$res2)
+plot(bats_nona$spBUILD, bats_nona$res2)
+plot(bats_nona$sD_TRE, bats_nona$res2)
+plot(predict(mod2, type='response'),jitter(bats_nona$OCC_PIPS, 0.05))
 dispZuur(mod2)
-plot(fitted(mod2),resid(mod2))
+sum(as.numeric(predict(mod2, type='response')>0.5)!=bats_nona$OCC_PIPS)/nrow(bats_nona)
+anova(mod1, mod2)
 
-mod3 <- glmer(PASSES ~ fSECTION*NOTURB + 
-                WINDS + I(WINDS^2) + DAYNO + TTMIDN + I(TTMIDN^2) + pTREE + D_LIN + 
-                (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-              family='poisson', data=bats)
-mod3a <- update(mod3, .~. -I(WINDS^2))
-anova(mod3, mod3a)
-# Drop wind speed poly
-mod3 <- mod3a
-mod3a <- update(mod3, .~. -I(TTMIDN^2))
-anova(mod3, mod3a)
-# Retain time to midnight poly.
+# Do we need SECTION*NOTURBS:
+mod2a <- update(mod2, .~. -fSECTION:NOTURB)
+anova(mod2a, mod2) # Borderline! Keep for now...
+
+# Do we need sMINTEMP?
+mod2a <- update(mod2, .~. -sMINTEMP)
+anova(mod2a, mod2) # No, try dropping this.
+
+mod3 <- mod2a
+summary(mod3)
+# Diagnostics:
+bats_nona$res3 <- resid(mod3, type='pearson')
+binnedplot(fitted(mod3), bats_nona$res3)
+plot(as.vector(bats_nona$fSECTION), bats_nona$res3)
+plot(bats_nona$NOTURB, bats_nona$res3)
+plot(bats_nona$sWINDS, bats_nona$res3)
+plot(bats_nona$sDAYNO, bats_nona$res3)
+plot(bats_nona$sTTMIDN, bats_nona$res3)
+plot(bats_nona$spBUILD, bats_nona$res3)
+plot(bats_nona$sD_TRE, bats_nona$res3)
+plot(predict(mod3, type='response'),jitter(bats_nona$OCC_PIPS, 0.05))
 dispZuur(mod3)
+sum(as.numeric(predict(mod3, type='response')>0.5)!=bats_nona$OCC_PIPS)/nrow(bats_nona)
+round(summary(mod3)$coef,3)
 
+# Point predictions:
+p_s1 <- data.frame(fSECTION=factor("1", levels=levels(bats_nona$fSECTION)),
+                   NOTURB=1,
+                   sWINDS=median(bats_nona$sWINDS),
+                   sDAYNO=median(bats_nona$sDAYNO),
+                   sTTMIDN=median(bats_nona$sTTMIDN),
+                   spBUILD=median(bats_nona$spBUILD),
+                   sD_TRE=median(bats_nona$sD_TRE),
+                   sTTMIDN2=median(bats_nona$sTTMIDN2),
+                   sDAYNO2=median(bats_nona$sDAYNO2)
+                   )
+p_s2 <- p_s1; p_s2$fSECTION <- factor("2", levels=levels(bats_nona$fSECTION))
+p_s3 <- p_s1; p_s3$fSECTION <- factor("3", levels=levels(bats_nona$fSECTION))
+p_s4 <- p_s1; p_s4$fSECTION <- factor("4", levels=levels(bats_nona$fSECTION))
+p_s5 <- p_s1; p_s5$fSECTION <- factor("5", levels=levels(bats_nona$fSECTION))
 
-display(mod4)
-dispZuur(mod4)
-mod4a <- update(mod4, .~., data=bats_nona)
-plot(fitted(mod4a), resid(mod4a))
-plot(bats_nona$fSECTION, resid(mod4a))
-plot(bats_nona$NOTURB, resid(mod4a))  # limited variation in 4 turbine sites
-plot(bats_nona$WINDS, resid(mod4a))  # Terrible resid pattern with wind speed!
-plot(bats_nona$DAYNO, resid(mod4a))
-plot(bats_nona$TTMIDN, resid(mod4a))
-plot(bats_nona$pTREE, resid(mod4a)) # Not so good in residuals with prop. trees
-plot(bats_nona$D_LIN, resid(mod4a)) # Terrible resid pattern with distance to lin. features
+predict(mod3, newdata=p_s1, type='response', re.form=NA)
+predict(mod3, newdata=p_s2, type='response', re.form=NA)
+predict(mod3, newdata=p_s3, type='response', re.form=NA)
+predict(mod3, newdata=p_s4, type='response', re.form=NA)
+predict(mod3, newdata=p_s5, type='response', re.form=NA)
 
-plot(bats$WINDS, bats$ALL_PIPS)
-
-
-
-library(mgcv)
-mod3 <- gamm()
->>>>>>> 5c2af5fe0d56033a23d248deebcb496845bb47b4
-
-par(mfrow=c(1,2))
-plot(predict(mod1),resid(mod1))
-hist(resid(mod1))
-sum(residuals(mod1)^2)/(nrow(bats)-7)
-
-library(glmmADMB)
-
-mod2 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), family='poisson', data=bats)
-plot(predict(mod2),resid(mod2))
-hist(resid(mod2))
-sum(residuals(mod2)^2)/(nrow(bats)-7)
-
-mod2a <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), 
-                  family='poisson', zeroInflation=T, data=bats)
-plot(predict(mod2a),resid(mod2a))
-hist(resid(mod2a))
-sum(residuals(mod2a)^2)/(nrow(bats)-7)
-
-mod3 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), family='nbinom', data=bats)
-plot(predict(mod3),resid(mod3))
-hist(resid(mod3))
-sum(residuals(mod3)^2)/(nrow(bats)-7)
-
-mod4 <- glmmadmb(PASSES ~ fSECTION + (1|SITE/TRSCT) + offset(log(AREA_ha)), family='nbinom1', data=bats)
-plot(predict(mod4),resid(mod4))
-hist(resid(mod4))
-sum(residuals(mod4)^2)/(nrow(bats)-7)
-
+# Point predictions w/o interaction between no. turbines/section AND w/o main effect of no. turbines:
+mod4 <- update(mod3, .~. -fSECTION:NOTURB)
+anova(mod4, mod3)
+mod4a <- update(mod4, .~. -NOTURB)
+anova(mod4a, mod4)
+mod4 <- mod4a
+p_s1a <- data.frame(fSECTION=factor("1", levels=levels(bats_nona$fSECTION)),
+                   sWINDS=median(bats_nona$sWINDS),
+                   sDAYNO=median(bats_nona$sDAYNO),
+                   sTTMIDN=median(bats_nona$sTTMIDN),
+                   spBUILD=median(bats_nona$spBUILD),
+                   sD_TRE=median(bats_nona$sD_TRE),
+                   sTTMIDN2=median(bats_nona$sTTMIDN2),
+                   sDAYNO2=median(bats_nona$sDAYNO2)
+)
+p_s2a <- p_s1a; p_s2a$fSECTION <- factor("2", levels=levels(bats_nona$fSECTION))
+p_s3a <- p_s1a; p_s3a$fSECTION <- factor("3", levels=levels(bats_nona$fSECTION))
+p_s4a <- p_s1a; p_s4a$fSECTION <- factor("4", levels=levels(bats_nona$fSECTION))
+p_s5a <- p_s1a; p_s5a$fSECTION <- factor("5", levels=levels(bats_nona$fSECTION))
+predict(mod4, newdata=p_s1a, type='response', re.form=NA)
+predict(mod4, newdata=p_s2a, type='response', re.form=NA)
+predict(mod4, newdata=p_s3a, type='response', re.form=NA)
+predict(mod4, newdata=p_s4a, type='response', re.form=NA)
+predict(mod4, newdata=p_s5a, type='response', re.form=NA)
